96014fd0fd55244b19c2fef7a5b7ceefdaa740fe
Ceronath <jordan_poulin10@hotmail.com>
2020-11-23 09:48:40 -0500

    done lab 4b

---
+++ src/main/java/Lab4b/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Author: Jordan Poulin 
+ * Date: Nov 16th 2020 
+ * Description: Car and person program
+ */
+package Lab4b;
+
+import Lab4.q3.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        App app=new App();
+        app.run();
+        
+    }
+}
